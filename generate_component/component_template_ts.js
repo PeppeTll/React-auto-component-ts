93422edae7c8220e406a810710f25c74c0c87769
PeppeTll <peppetll@gmail.com>
2023-07-01 13:39:09 +0200

    chore: added flag type

--- generate_component/component_template_ts.js
+++ generate_component/component_template_ts.js
@@ -1,15 +1,17 @@
-// index.css
+// index.scss
 export function index(name) {
   return `.${name} {
     /* add your style  */
   }`;
 }
 
-// component.js
+// component.tsx
 export function component(name) {
-  return `import './${name}.css';
+  return `import './${name}.scss';
 
-const ${name} = () => {
+// interface ${name}Props {} Add your props type
+
+const ${name} = ({}: ${name}Props) => {
   return (
     <div className="${name}">Hello 👋, I am a ${name} component.</div>
   );
@@ -19,9 +21,9 @@ export default ${name};
 `;
 }
 
-// index.js
+// index.tsx
 export function barrel(name) {
-  return `import ${name} from './${name}.jsx';
+  return `import ${name} from './${name}.tsx';
 
 export default ${name};
 `;
--- generate_component/index.js
+++ generate_component/index.js
@@ -1,7 +1,8 @@
 import { existsSync, mkdirSync, writeFile } from 'fs';
 import { index, component, barrel } from './component_templates.js';
+import { index as indexTSX, component as componentTSX, barrel as barrelTSX } from './component_templates_tsx.js';
 
-const [name] = process.argv.slice(2);
+const [name, fileType = 'jsx'] = process.argv.slice(2);
 if (!name) throw new Error('You must include a component name.');
 
 const componentsDir = './src/components/';
@@ -26,14 +27,28 @@ function writeFileErrorHandler(err) {
   if (err) throw err;
 }
 
-// component.tsx
-writeFile(`${dir}/${name}.tsx`, component(name), writeFileErrorHandler);
-console.log(`Component file "${name}.tsx" created.`);
+// Determine the templates based on the file type
+let templates;
+if (fileType === 'tsx') {
+  templates = {
+    index: indexTSX,
+    component: componentTSX,
+    barrel: barrelTSX,
+  };
+} else {
+  templates = {
+    index,
+    component,
+    barrel,
+  };
+}
 
-// component.scss
-writeFile(`${dir}/${name}.scss`, index(name), writeFileErrorHandler);
+// Write component files
+writeFile(`${dir}/${name}.${fileType}`, templates.component(name), writeFileErrorHandler);
+console.log(`Component file "${name}.${fileType}" created.`);
+
+writeFile(`${dir}/${name}.scss`, templates.index(name), writeFileErrorHandler);
 console.log(`Component file "${name}.scss" created.`);
 
-// index.jsx
-writeFile(`${dir}/index.tsx`, barrel(name), writeFileErrorHandler);
-console.log(`Component file "index.tsx" created.`);
+writeFile(`${dir}/index.${fileType}`, templates.barrel(name), writeFileErrorHandler);
+console.log(`Component file "index.${fileType}" created.`);
